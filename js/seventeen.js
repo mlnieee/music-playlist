024aad1732480fa860dcc8253caa12bab5bd600f
mlnieee <167530696+mlnieee@users.noreply.github.com>
2024-04-26 01:34:22 +0800

    Add files via upload

---
+++ js/seventeen.js
@@ -0,0 +1,143 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const audio=document.getElementById("audio");
+    const playPauseButton=document.getElementById("play-pause");
+    const prevButton=document.getElementById("back");
+    const nextButton=document.getElementById("next");
+    const volumeControl=document.getElementById("vol");
+    const trackSlider=document.getElementById("track-slider");
+    const currentTimeDisplay=document.getElementById("current-time");
+    const totalDurationDisplay=document.getElementById("total-duration");
+    const trackNameDisplay=document.getElementById("nameDisplay");
+    const albumPhoto=document.getElementById("album-photo");
+
+    let isPlaying = true;
+    let currentTrack = 0;
+    let audioPosition = 0;
+
+    const trackList=[
+        "songs/seventeen/Pretty U.mp3",
+        "ssongs/seventeen/Healing.mp3",
+        "ssongs/seventeen/To You.mp3",
+        "songs/seventeen/Rock With You.mp3",
+        "songs/seventeen/Darling.mp3",
+        "songs/seventeen/Hot.mp3",
+        "songs/seventeen/Circles.mp3",
+        "songs/seventeen/WORLD.mp3",
+        "songs/seventeen/CHEERS.mp3",
+        "songs/seventeen/japanese.mp3",
+        "songs/seventeen/God Of Music.mp3",
+        "songs/seventeen/Yawn.mp3",
+        "songs/seventeen/Headliner.mp3",
+    ];
+
+    const albumPhotos=[
+        "img/pretty u.avif",
+        "img/healing.avif",
+        "img/to you.avif",        
+        "img/to you.avif",
+        "img/darling.avif",
+        "img/darling.avif",
+        "img/circles.avif",
+        "img/circles.avif",
+        "img/circles.avif",
+        "img/japanese.avif",
+        "img/god of music.avif",
+        "img/god of music.avif",
+        "img/god of music.avif",
+    ];
+
+    function togglePlayPause(){
+        if (!isPlaying){
+            if (audioPosition === 0){
+                audio.src=trackList[currentTrack];
+            }
+            audio.load();
+            audio.currentTime=audioPosition;
+            audio
+            .play()
+            .then(( ) => {
+                playPauseButton.textContent="▐▐ ";
+                isPlaying = true;
+                updatenameDisplay(currentTrack);
+            })
+            .catch((error) => {
+                console.error("Audio Playback Error: " + error.message);
+            });
+        } else {
+            audioPosition = audio.currentTime;
+            audio.pause();
+            playPauseButton.textContent = "▶";
+            isPlaying=false;
+        }
+    }
+
+    playPauseButton.addEventListener("click", togglePlayPause);
+
+    nextButton.addEventListener("click",function(){
+        if(currentTrack < trackList.length - 1){
+            currentTrack++;
+        } else {
+            currentTrack=0;
+        }
+        playTrack(currentTrack);
+    });
+
+    prevButton.addEventListener("click", function (){
+        if (currentTrack >  0){
+            currentTrack--;
+        } else {
+            currentTrack = trackList.length - 1;
+        }
+        playTrack(currentTrack);
+    });
+
+    function playTrack(trackIndex){
+        audio.src=trackList[trackIndex];
+        audio.load();
+        audio.play();
+        playPauseButton.textContent="▐▐";
+        isPlaying=true;
+        updateTrackName(trackIndex);
+    }
+
+    function updateTrackName(trackIndex){
+        const trackName=trackList[trackIndex];
+        const cleanedTrackName = trackName.replace("songs/itzy/","");
+        trackNameDisplay.textContent=cleanedTrackName;
+        albumPhoto.src = albumPhotos[trackIndex];
+    }
+
+    volumeControl.addEventListener("input", function (){
+        audio.volume = volumeControl.value;
+    });
+
+    audio.addEventListener("timeupdate", function() {
+        const currentTime = formatTime(audio.currentTime);
+        const totalDuration = formatTime(audio.duration);
+        currentTimeDisplay.textContent = currentTime;
+        totalDurationDisplay.textContent = totalDuration;
+
+        const position = (audio.currentTime / audio.duration) * 100;
+        trackSlider.value = position;
+    });
+
+    trackSlider.addEventListener("input", function() {
+        const newPosition = (trackSlider.value / 100) * audio.duration;
+        audio.currentTime = newPosition;
+    });
+
+    audio.addEventListener("ended", function () {
+        if (currentTrack < trackList.length - 1) {
+            currentTrack++;
+        } else {
+            currentTrack=0;
+        }
+        playTrack(currentTrack);
+    });
+
+    function formatTime(seconds) {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = Math.floor(seconds % 60);
+        return `${minutes}:${remainingSeconds < 10? "0" : ""}${remainingSeconds}`;
+    }
+})
